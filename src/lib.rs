ef1546cecf4f6ac566b44245a3af44a78473a081
Phillip <phillipv@Mac.optusnet.com.au>
2024-11-22 22:40:14 +1100

    removed bloat

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod app;
-pub mod caching;
-pub mod parsing;
-pub mod styles;
-pub mod ui;
-pub mod utils;
-pub mod widgets;
-pub mod wikipedia;
--- src/widgets/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod scrollbar;
-pub use scrollbar::ScrollBar;
